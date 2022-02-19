ad9a90fe68426b3ad460377ef9002ee38066fee4
dkovalenkoqa <kovalenkodima715@gmail.com>
2022-02-19 17:11:04 +0200

    start crud lesson

---
+++ crud/main.py
@@ -0,0 +1,17 @@
+from create_1 import create_user
+
+while True:
+
+    action = input('Please enter action ')
+
+    if action == 'create':
+        print('action = ', action)
+    elif action == 'read':
+        print('action = ', action)
+    elif action == 'update':
+        print('action = ', action)
+    elif action == 'delete':
+        print('action = ', action)
+    else:
+        print('Please enter create or read, or update, or delete')
+
